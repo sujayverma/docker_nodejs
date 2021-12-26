20c520808ef77bfd1857792eae9bb553f9aeca28
sujayverma <sujayverma124@gmail.com>
2021-12-26 15:40:10 +0530

    Intial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h2> Hell Yeah!!!</h2>')
+});
+
+const port = process.env.port || 3000
+
+app.listen(port, () => console.log(`listening on port ${port}`))
